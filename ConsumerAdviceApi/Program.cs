8e84d37bf7c72a4de6faa39fc8acacfc785d5b5d
Davi Lucas dos Santos Oliveira <56208920+DaviLucasOliveira@users.noreply.github.com>
2026-09-11 02:01:37 -0300

    Add files via upload

---
+++ ConsumerAdviceApi/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsumerAdviceApi.Models;
+using static System.Console;
+
+var enderecoUrl = "https://api.adviceslip.com/advice";
+
+WriteLine($"Consultando o endereço na url: {enderecoUrl}");
+
+var client = new HttpClient();
+
+try
+{
+    HttpResponseMessage response = await client.GetAsync(enderecoUrl);
+    response.EnsureSuccessStatusCode();
+
+    string respostaApi = await response.Content.ReadAsStringAsync();
+
+    AdviceResponse? advice = System.Text.Json.JsonSerializer.Deserialize<AdviceResponse>(respostaApi);
+
+    WriteLine($"\nToday's advice:\n" + advice?.Slip?.Advice);
+
+}
+catch (Exception e)
+{
+    WriteLine($"Ocorreu um erro ao consultar o endereço: {e.Message}");
+}
